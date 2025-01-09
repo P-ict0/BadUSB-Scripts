edcf8999456dd21e1ca29fb986f5fabcd8ece4d2
Rodrigo Martín Núñez <83213317+P-ict0@users.noreply.github.com>
2025-01-09 16:59:48 +0100

    (Payload) Francer

---
+++ helpers/francer/francer.ps1
@@ -0,0 +1,24 @@
+$imageUrl = "https://raw.githubusercontent.com/P-ict0/BadUSB-Scripts/refs/heads/main/helpers/francer/france.png?raw=true"
+$outputPath = Join-Path -Path $env:TEMP -ChildPath 'france.png'
+
+Invoke-WebRequest -Uri $imageUrl -OutFile $outputPath
+
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+    
+     public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+         
+         public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code
+[Win32.Wallpaper]::SetWallpaper($outputPath)
+
+Remove-Item -Path $outputPath
